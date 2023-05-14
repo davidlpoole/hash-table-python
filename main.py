fbec7e0e32bb428954abc971a369c09b68aa2703
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-05-14 16:50:25 +1000

    Start adding tests

--- main.py
+++
@@ -1,6 +0,0 @@
-def hash_function(text):
-    return sum(ord(character) for character in text)
-
-
-if __name__ == '__main__':
-    print(hash_function("Lorem"))
---
+++ test_hashtable.py
@@ -0,0 +1,5 @@
+from hashtable import HashTable
+
+
+def test_should_create_hashtable():
+    assert HashTable() is not None
